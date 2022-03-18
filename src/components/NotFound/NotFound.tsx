957ab7d8869739e58bd0adf99d4dbd90fbe7820f
MalcolmDahling <mac.dahling@gmail.com>
2022-03-18 14:57:35 +0100

    asdf

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,4 @@
+export function NotFound(){
+
+    return(<p>Page could not be found, this is all your fault!</p>);
+}
---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { NotFound } from './components/NotFound/NotFound';
+import { Animal } from './components/Animal/Animal';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/animal/:id" element={<Animal />}></Route>
+                <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
