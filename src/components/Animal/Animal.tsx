7ce5659e127df71e1c64b70df70e8828e610358c
MalcolmDahling <mac.dahling@gmail.com>
2022-03-19 00:27:32 +0100

    asdf

--- src/components/Animal/Animal.tsx
+++ src/components/Animal/Animal.tsx
@@ -1,44 +1,53 @@
+import axios from "axios";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { IAnimal } from "../../models/IAnimal";
-import { AnimalService } from "../services/AnimalService";
 
 import './Animal.css';
 
 export function Animal(){
 
-    AnimalService();
-
-    let animals:IAnimal[] = JSON.parse( localStorage.getItem('animals') || '' );
-
     let {id} = useParams();
 
-    let animalHtml;
+    const [animals, setAnimals] = useState([]);
+
+    axios.get('https://animals.azurewebsites.net/api/animals')
+        .then(response => {
+            if(!localStorage.getItem('animals')){
+                localStorage.setItem('animals', JSON.stringify(response.data));
+            }
 
 
-    for(let i = 0; i < animals.length; i++){
+            setAnimals( JSON.parse( localStorage.getItem('animals') || '' ).map( (animal:IAnimal, i:number) => {
         
-
-        if(animals[i].id == +id!){
-
-            animalHtml = (
-                <div className="animal2">
-                    <p>{animals[i].name}</p>
-                    <img src={animals[i].imageUrl}></img>
-                    <p>Latinskt namn: {animals[i].latinName}</p>
-                    <p>Född: {animals[i].yearOfBirth}</p>
-                    <p>Beskrivning: {animals[i].longDescription}</p>
-                    <p>Medicin: {animals[i].medicine}</p>
-                    <p>Har ätit: { animals[i].isFed ? 'Ja' : 'Nej' }</p>
-                    <p>Senast ätit: {animals[i].lastFed}</p>
-                </div>
-            );
+                if(i+1 == +id!){
+                    return(
+                        <div className="animal2" key={i}>
+                            <p>{animal.name}</p>
+                            <img src={animal.imageUrl}></img>
+                            <p>Latinskt namn: {animal.latinName}</p>
+                            <p>Född: {animal.yearOfBirth}</p>
+                            <p>Beskrivning: {animal.longDescription}</p>
+                            <p>Medicin: {animal.medicine}</p>
+                            <p>Har ätit: { animal.isFed ? 'Ja' : 'Nej' }</p>
+                            <p>Senast ätit: {animal.lastFed}</p>
+                        </div>
+                    );
+                }
+                
+            }));
         }
-    }
+    );
+
+
+
+
+
 
 
     return(
         <div className="wrapper2">
-            {animalHtml}
+            {animals}
         </div>
     );
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,21 +1,35 @@
+import axios from "axios";
+import { useState } from "react";
 import { IAnimal } from "../../models/IAnimal";
-import { AnimalService } from "../services/AnimalService";
 import './Home.css';
 
 export function Home(){
 
-    AnimalService();
+    const [animals, setAnimals] = useState([]);
 
-    let animals = JSON.parse( localStorage.getItem('animals') || '' ).map( (animal:IAnimal) => {
-        
-        return(
-            <a key={animal.id} className="animal" href={"animal/" + animal.id}>
-                <p>{animal.name}</p>
-                <img src={animal.imageUrl}></img>
-                <p>{animal.shortDescription}</p>
-            </a>
-        );
+
+    
+    axios.get('https://animals.azurewebsites.net/api/animals')
+    .then(response => {
+        if(!localStorage.getItem('animals')){
+            localStorage.setItem('animals', JSON.stringify(response.data));
+        }
+
+
+        setAnimals( JSON.parse( localStorage.getItem('animals') || '' ).map( (animal:IAnimal) => {
+    
+            return(
+                <a key={animal.id} className="animal" href={"animal/" + animal.id}>
+                    <p>{animal.name}</p>
+                    <img src={animal.imageUrl}></img>
+                    <p>{animal.shortDescription}</p>
+                </a>
+            );
+        }));
     });
+    
+
+
 
 
     return(
